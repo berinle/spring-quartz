cef2ad3e6e284e9e245bf19d16a00d61db239fdd
Bayo Erinle <berinle@gmail.com>
2012-03-23 12:00:48 -0400

    added simple main driver class

---
+++ src/test/java/com/berinle/sq/MainDriver.java
@@ -0,0 +1,20 @@
+package com.berinle.sq;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainDriver {
+
+	public static void main(String[] args) {
+        //build the spring context
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"appContext.xml"});
+        //get hold of a bean
+
+        //print bean info
+        Object emailJob = ctx.getBean("emailJob");
+        System.out.println("emailJob: " + emailJob);
+
+        Object jobTrigger = ctx.getBean("jobTrigger");
+        System.out.println("jobTrigger = " + jobTrigger);
+
+    }
+}
