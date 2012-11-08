e4528c71079296eb84ba2740276b54e204b58aff
Bayo Erinle <berinle@gmail.com>
2012-11-08 10:25:47 -0500

    cleaned up code - jmx monitoring now on

--- src/main/java/com/berinle/sq/listener/AppContextListener.java
+++ src/main/java/com/berinle/sq/listener/AppContextListener.java
@@ -11,24 +11,40 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.jmx.support.MBeanServerFactoryBean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AppContextListener implements ApplicationListener {
     private static Log log = LogFactory.getLog(AppContextListener.class);
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
-        ContextRefreshedEvent e = (ContextRefreshedEvent) event;
 
-        ApplicationContext context = e.getApplicationContext();
-        EhCacheCacheManager ehCacheCacheManager = context.getBean(EhCacheCacheManager.class);
-        if(null != ehCacheCacheManager){
-            CacheManager cacheManager = ehCacheCacheManager.getCacheManager();
-            ManagementService managementService = context.getBean(ManagementService.class);
-            if(null != managementService){
-                MBeanServerFactoryBean mBeanServerFactoryBean = context.getBean(MBeanServerFactoryBean.class);
-                managementService = new ManagementService(cacheManager, mBeanServerFactoryBean.getObject(), true, true, true, true);
+        if(event instanceof ContextRefreshedEvent){
+            ContextRefreshedEvent e = (ContextRefreshedEvent) event;
 
-                log.info("Done resetting management service bean!");
+            ApplicationContext context = e.getApplicationContext();
+            String[] activeProfiles = context.getEnvironment().getActiveProfiles();
+            boolean flag = false;
+            for (String activeProfile : activeProfiles) {
+                if(activeProfile.equals("stats"))
+                    flag = true;
+            }
+
+            if(flag){
+                EhCacheCacheManager ehCacheCacheManager = context.getBean(EhCacheCacheManager.class);
+                if(null != ehCacheCacheManager){
+                    CacheManager cacheManager = ehCacheCacheManager.getCacheManager();
+                    ManagementService managementService = context.getBean(ManagementService.class);
+                    if(null != managementService){
+                        MBeanServerFactoryBean mBeanServerFactoryBean = context.getBean(MBeanServerFactoryBean.class);
+                        managementService = new ManagementService(cacheManager, mBeanServerFactoryBean.getObject(), true, true, true, true);
+                        managementService.init(); //start it
+
+                        log.info(String.format("management service: %s", managementService));
+                        log.info("Done resetting management service bean!");
+                    }
+                }
             }
         }
     }
